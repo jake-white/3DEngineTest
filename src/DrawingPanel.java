b803cefad1e9c426b49d90ec52bad72354d62356
jake-white <acorns1225>
2015-06-05 21:59:47 -0400

    Repo Creation

---
+++ src/DrawingPanel.java
@@ -0,0 +1,13 @@
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+
+public class DrawingPanel extends JPanel{
+	
+	@Override
+	public void paintComponent(Graphics g)
+	{
+		
+	}
+}
---
+++ src/Point3D.java
@@ -0,0 +1,11 @@
+
+public class Point3D {
+
+	int x, y, z;
+	public Point3D(int x, int y, int z)
+	{
+		this.x = x;
+		this.y = x;
+		this.z = x;
+	}
+}
